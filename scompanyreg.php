c7a03d5d457c96fe4ab4572d04a49d66df01942a
Shashank Rao <rshashank192@gmail.com>
2013-09-22 09:59:42 +0530

    Added Company Register (Student)

---
+++ scompanyreg.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2>Register For Companies (Will be dynamic later)</h2>
+<h3>Check eligibility and render this dynamically</h3>
+<br>
+<table border = "border" cellpadding="6">
+	<tr>
+			<th>Company Name</th>
+			<th>Visit Date/Interview Date</th>
+			<th>Register</th>
+	</tr>
+	<tr>
+			<td>LinkedIn</td>
+			<td>07/10/2013 (10th October)</td>
+			<td><button>Register</button></td>
+	</tr>
+		<tr>
+			<td>SAP</td>
+			<td>XX/10/2013 (XX October)</td>
+			<td><button>Register</button></td>
+	</tr>
+</table>	
+</body>
+</html>
