eaa6c16fd782c4dc9661835127c30027a01af462
sandeep <r.v_sandeep@yahoo.in>
2013-09-24 21:52:26 +0530

    removed password from sdetailsadd.php

--- includes/constants.php
+++ includes/constants.php
@@ -3,7 +3,7 @@
 // Database Constants
 define("DB_SERVER", "localhost");
 define("DB_USER", "root");
-define("DB_PASS", "");
+define("DB_PASS", "sqlpass");
 define("DB_NAME", "placementupdates");
 
 ?>
--- sdetailsadd.php
+++ sdetailsadd.php
@@ -14,10 +14,6 @@
 <label for='email' >Email Address:</label>
 <input type='text' name='email' id='email' maxlength="50" tabindex="2" tabindex="2"/><br><br>
 
-<label for="pass">Enter Password:</label>
-<input type="password" name="pass" id="pass" tabindex="3"> <br><br>
-<span id="passstrength"></span>
-
 <label for='phone' >Phone Number:</label>
 <input type='text' name='phone' id='phone' maxlength="15" tabindex="4"/><br><br>
 
