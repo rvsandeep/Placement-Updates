0d5dda435a81a8c741a4409486ad81595e36d02e
coldmix <rshashank192@gmail.com>
2013-11-19 16:36:15 +0530

    added register pages

--- register.php
+++ register.php
@@ -1,23 +1,58 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+	<meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        
 	<title>
 		REGISTER | Placement-Updates
 	</title>
+
+	 <link rel="shortcut icon" href="../favicon.ico"> 
+     <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+		<style>
+			@import url(http://fonts.googleapis.com/css?family=Ubuntu:400,700);
+			body {
+			
+				-webkit-background-size: cover;
+				-moz-background-size: cover;
+				background-size: cover;
+			}
+			.container > header h1,
+			.container > header h2 {
+				color: #000;
+				text-shadow: 0 1px 1px rgba(0,0,0,0.7);
+			}
+}
+
+		</style>
+    
 </head>
 <body>
-<h2>Registration Form</h2>
-<form method="post" action="registersubmit.php">
-<h2>Personal Details:</h2><br>
+	<div class="container">
 
-<label for='username' >Email Address:</label>
+			<header>
+<h1>Registration Form</h1>
+
+			</header>
+<section class="main">
+<form method="post" action="registersubmit.php"  class="form-3">
+	<p class="clearfix">
+<label for='username' >Email Address: </label>
 <input type='text' name='username' id='username' maxlength="50" tabindex="2" tabindex="2"/><br><br>
-
-<label for="password">Enter Password:</label>
+</p>
+<p class="clearfix">
+<label for="password">Enter Password: </label>
 <input type="password" name="password" id="password" tabindex="3">
-<br><br>
-<input type="submit" value="Register" tabindex="18">
-
+</p>
+<p class="clearfix">
+	<center>
+<input type="submit" value="Register">
+</center>
+</p>
 </form>
-<p>
+</section>
+</div>
 </body>
 </html>
--- registersubmit.php
+++ registersubmit.php
@@ -1,20 +1,49 @@
+<html>
+<head>
+<meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        
+<link rel="shortcut icon" href="../favicon.ico"> 
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+		
+	<title>
+		REGISTER | Placement-Updates
+	</title>
+
+		<style>
+			@import url(http://fonts.googleapis.com/css?family=Ubuntu:400,700);
+			body {
+			
+				-webkit-background-size: cover;
+				-moz-background-size: cover;
+				background-size: cover;
+			}
+			.container > header h1,
+			.container > header h3 {
+				color: #000;
+				text-shadow: 0 1px 1px rgba(0,0,0,0.7);
+			}
+}
+
+		</style>
+
+</head>
+<body>
+	<div class="container">
+	<header>
+<h1>Registration Process:</h1>
+
+<br><br>
+<h1>
 <?php
 	session_start();
 	require_once("includes/connection.php");
 	require_once("includes/functions.php");
 // Query the database for the username and password
 	global $connection;
-	echo "
-<html>
-<head>
-	<title>
-		REGISTER | Placement-Updates
-	</title>
-</head>
-<body>
-<h2>Registration Form</h2>
-<br><br>
-";
+	
+
 
 		$username = $_POST['username'];
 		$password = $_POST['password'];
@@ -26,7 +55,9 @@
 
 		if (mysqli_num_rows($result_set) == 1) {
 			echo "User already exist</br>Enter a different username";
-			redirect_to("register.php");
+			//redirect_to("register.php");
+			echo " and try Again.<br><br>  <a href='register.php'>Register</a></br></p>";
+				
 		} else {
 			$hashed_password = sha1($password);
 			$cred = 0;
@@ -35,11 +66,13 @@
 			if ($result) {
 				echo "<p>The user was successfully created.<br><br> You can login now.<br><br> <a href='login.php'>Login</a></br><p>";
 			} else {
-				echo "<p>The user could not be created.<br><br>  Try Again.<br><br>  <a href='register.php'>Register</a></br></p>";
+				echo "<p>The user could not be created.<br><br> Please Try Again.<br><br>  <a href='register.php'>Register</a></br></p>";
 				
 			}
 		}
-echo "
+echo "</h1>
+</header>
+</div>
 </body>
 </html>";
 
